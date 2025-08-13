bdd8e141b1194952aab0ec9d2032b3968d51fe5a
Amasha Das <125284502+amasha-d@users.noreply.github.com>
2025-08-13 16:36:47 -0500

    typed vchanges
    hope it works

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,81 +1,98 @@
 $(function () {
-
-    // Header Scroll
-    $(window).scroll(function () {
-        if ($(window).scrollTop() >= 60) {
-            $("header").addClass("fixed-header");
-        } else {
-            $("header").removeClass("fixed-header");
-        }
-    });
-
-
-    // Featured Owl Carousel
-    $('.featured-projects-slider .owl-carousel').owlCarousel({
-        center: true,
-        loop: true,
-        margin: 30,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        autoplayTimeout: 5000,
-        autoplayHoverPause: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 2
-            },
-            1000: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            }
-        }
-    })
-
-
-    // Count
-    $('.count').each(function () {
-		$(this).prop('Counter', 0).animate({
-			Counter: $(this).text()
-		}, {
-			duration: 1000,
-			easing: 'swing',
-			step: function (now) {
-				$(this).text(Math.ceil(now));
-			}
-		});
-	});
-
-
-    // ScrollToTop
-    function scrollToTop() {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
+  // Header Scroll
+  $(window).scroll(function () {
+    if ($(window).scrollTop() >= 60) {
+      $("header").addClass("fixed-header");
+    } else {
+      $("header").removeClass("fixed-header");
     }
+  });
 
-    const btn = document.getElementById("scrollToTopBtn");
-    btn.addEventListener("click", scrollToTop);
+  /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector(".typed");
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
 
-    window.onscroll = function () {
-        const btn = document.getElementById("scrollToTopBtn");
-        if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
-            btn.style.display = "flex";
-        } else {
-            btn.style.display = "none";
+  // Featured Owl Carousel
+  $(".featured-projects-slider .owl-carousel").owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  // Count
+  $(".count").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 1000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
         }
-    };
+      );
+  });
 
+  // ScrollToTop
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 
-    // Aos
-	AOS.init({
-		once: true,
-	});
+  const btn = document.getElementById("scrollToTopBtn");
+  btn.addEventListener("click", scrollToTop);
 
+  window.onscroll = function () {
+    const btn = document.getElementById("scrollToTopBtn");
+    if (
+      document.documentElement.scrollTop > 100 ||
+      document.body.scrollTop > 100
+    ) {
+      btn.style.display = "flex";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  // Aos
+  AOS.init({
+    once: true,
+  });
 });
-
